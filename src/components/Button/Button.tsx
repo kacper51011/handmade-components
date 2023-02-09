b2d3ac2c5d255bd8f26f4887fe7a94913fd68e4d
Kacper <kacper.tylec1999@gmail.com>
2023-02-09 17:56:36 +0100

    initialized Button custom component styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,44 @@
-import React from "react";
-import "./button.css";
+import React, { FC } from "react";
+import styled from "styled-components";
 import { ButtonProps } from "./Button.types";
 
-const Button = ({}: ButtonProps) => {
-  return <button></button>;
+const StyledButton = styled.button<ButtonProps>`
+  ${({ fullWidth }) => fullWidth && "width:100%;"}
+  padding: ${({ size }) =>
+    (size === "small" && "0.35rem 0.8rem;") ||
+    (size === "medium" && "0.55rem 1.4rem;") ||
+    (size === "large" && "1rem 3rem;")}
+  background: ${({ color, variant }) =>
+    (color && color + ";") ||
+    (variant === "primary" && "#00e676;") ||
+    (variant === "secondary" && "#ffea00;")}
+  border-radius: ${({ sharpness }) =>
+    sharpness === "primary" ? "4px;" : "16px;"}
+    font-family: Arial, Helvetica, sans-serif;
+
+  border: 0;
+  font-weight: 700;
+`;
+const Button: FC<ButtonProps> = ({
+  fullWidth = false,
+  size = "medium",
+  sharpness = "primary",
+  variant = "primary",
+  text,
+  ...props
+}) => {
+  return (
+    <StyledButton
+      fullWidth={fullWidth}
+      size={size}
+      sharpness={sharpness}
+      variant={variant}
+      // color={color}
+      {...props}
+    >
+      {text}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,9 +1,9 @@
-import { ComponentProps } from "react";
+import { ComponentPropsWithRef } from "react";
 
 export type ButtonProps = {
   text?: string;
-  variant?: "primary" | "secondary";
-  sharpness?: "sharp" | "rounded";
-  size?: "small" | "medium" | "large";
-  fullWidth?: boolean;
-} & ComponentProps<"button">;
+  variant: "primary" | "secondary";
+  sharpness: "primary" | "rounded";
+  size: "small" | "medium" | "large";
+  fullWidth: boolean;
+} & ComponentPropsWithRef<"button">;
